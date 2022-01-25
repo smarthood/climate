bd0022c0d14bf3249ef4664f64e622fbfc72da1b
smarthood <renolddickson18@gmail.com>
2022-01-25 17:27:14 +0530

    commit description

---
+++ src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Chat() {
+  return <div>
+      <h1>Chat</h1>
+  </div>;
+}
---
+++ src/components/Join.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Join() {
+  return <div>
+      <h1>Join</h1>
+  </div>;
+}
